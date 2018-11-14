b1f6cbe596420e3b6884ac62660775fb1a106a24
startwritingcode <derek@startwritingcode.com>
2018-11-14 09:46:34 -0600

    Create Lawn and LawnsList endpoints. Remove HelloWorld endpoint. Closes #2

--- app/api.py
+++ app/api.py
@@ -4,11 +4,26 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Lawn(Resource):
+    def get(self, lawn_id):
+        print('Getting lawn with id: %s' % lawn_id)
+
+    def put(self, lawn_id):
+        print('Update lawn with id: %s' % lawn_id)
+    
+    def delete(self, lawn_id):
+        print('Deleting lawn with id: %s' % lawn_id)
+
+class LawnList(Resource):
     def get(self):
-        return {'hello': 'world'}
+        print('Getting all lawns')
+
+    def post(self):
+        print('Creating a new lawn')
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(LawnList, '/lawns')
+api.add_resource(Lawn, '/lawns/<lawn_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
