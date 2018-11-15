654be1c2366ee88a0a2c827adb16c93c0bff8b3e
startwritingcode <derek@startwritingcode.com>
2018-11-15 10:24:41 -0600

    Add Lawn and Address models. Closes #3

---
+++ models/address.py
@@ -0,0 +1,8 @@
+class Address:
+    def __init__(self, address1, address2, city, state, zip, country):
+        self.address1 = address1
+        self.address2 = address2
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.country = country
---
+++ models/lawn.py
@@ -0,0 +1,6 @@
+class Lawn:
+    def __init__(self, name, address, total_area, unit_of_measurement):
+        self.name = name
+        self.address = address
+        self.total_area = total_area
+        self.unit_of_measurement = unit_of_measurement
