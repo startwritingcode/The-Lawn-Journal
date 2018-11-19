44ced535527838573072cd6c264c2bfb166d56cd
startwritingcode <derek@startwritingcode.com>
2018-11-19 10:26:02 -0600

    Retrieve LawnList from sample data in API. Closes #7

--- client/src/components/LawnList/LawnList.js
+++ client/src/components/LawnList/LawnList.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import './LawnList.css';
 
 class LawnList extends Component {
+
+  constructor() {
+      super();
+      this.state = {
+          lawns: []
+      }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/lawns')
+    .then(results => {
+        return results.json()
+    }).then(data => {
+        let lawns = data.lawns.map((lawn) => {
+            return (
+                <li key={Math.floor(Math.random() * 100)}>{lawn.name}</li>
+            )
+        })
+        this.setState({lawns: lawns});
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Lawn List</h2>
-        <ul>
-            <li>Home Lawn</li>
-            <li>Parents Lawn</li>
-            <li>Vacation Home</li>
-        </ul>
+        <ul>{this.state.lawns}</ul>
       </div>
     );
   }
--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from app.resources.lawn import LawnApi
 from app.resources.lawnlist import LawnListApi
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = Api(app)
 
-api.add_resource(LawnListApi, '/lawns')
-api.add_resource(LawnApi, '/lawns/<lawn_id>')
+api.add_resource(LawnListApi, '/api/lawns')
+api.add_resource(LawnApi, '/api/lawns/<lawn_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
