ae7d94ee586c05c450cf276fda4ddd68f66a2f60
startwritingcode <derek@startwritingcode.com>
2018-11-19 12:10:51 -0600

    Install Flask-PyMongo and update requirements.txt

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 
 from app.resources.lawn import LawnApi
 from app.resources.lawnlist import LawnListApi
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/lawnjournal"
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = Api(app)
+mongo = PyMongo(app)
 
 api.add_resource(LawnListApi, '/api/lawns')
 api.add_resource(LawnApi, '/api/lawns/<lawn_id>')
